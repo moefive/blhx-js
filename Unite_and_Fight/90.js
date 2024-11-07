98e561c41cf3d77aa68cb3e88ae1d746dc4e44a9
Yagami <41063544+moefive@users.noreply.github.com>
2024-11-08 05:10:46 +0800

    Update 90.js

--- Unite_and_Fight/90.js
+++ Unite_and_Fight/90.js
@@ -37,7 +37,7 @@
                   window.location.replace(`https://game.granbluefantasy.jp/#${data.option.result_data.url}`)
               }else {
 //                 跳转到url
-                     location.hash = 'quest/supporter/917861/1/0/10116'
+                     location.hash = 'quest/supporter/924561/1/0/10116'
               }
           }
         })
